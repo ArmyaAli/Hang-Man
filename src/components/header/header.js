2948103d588b531316c49ced707c3265be89b848
armyaali <ali.umar.work@gmail.com>
2020-11-07 17:27:00 -0500

    restructured the project folder

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import './header.css';
+
+function Header() {
+  return (
+    <div className="header-container">
+        <span className="header-text">Ali and Ringo's Hangman Game</span>
+    </div>
+  );
+}
+
+export default Header;
