e75f6d12d9f545b1586c0a237ef5ec7553486568
armyaali <ali.umar.work@gmail.com>
2020-11-07 18:46:46 -0500

    length and width attributes for image

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,12 +1,14 @@
 import Header from '../header/header'
 import React from 'react';
 import '../../style/game.css'
+
 class Game extends React.Component {
+
     render() {
       return (
         <div class="page-container">
             <Header />
-            <canvas className="game-canvas"></canvas>
+            <canvas id="canvas" className="game-canvas" height="600" width="600"></canvas>
         </div>
       );
     }
